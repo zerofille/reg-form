54d4b2507303d2e8890d14694b45dbc5dbc5e665
zerofille <nstrdnv@gmai.com>
2021-11-04 22:15:59 +0200

    Add fix to validation

--- src/Pages/Login-form/index.jsx
+++ src/Pages/Login-form/index.jsx
@@ -1,23 +1,22 @@
-import React from "react";
 import styles from "./style.module.scss";
 import React, { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import{LOGIN_SCHEME} from '../../Utils/Validation-schemas';
+import { LOGIN_SCHEME } from "../../Utils/Validation-schemas";
 import cx from "classnames";
-const initialValues={
-  login:"",
-  password:"",
-  isRemembered:false
-}
+const initialValues = {
+  login: "",
+  password: "",
+  isRemembered: false,
+};
 
 function LoginForm(props) {
   const submitForm = (values, formikBag) => formikBag.resetForm();
 
-//   const validStyle = ()=>{
-// cx({
-//   [styles.field]:
-// })
-//   }
+  //   const validStyle = ()=>{
+  // cx({
+  //   [styles.field]:!
+  // })
+  //   }
   return (
     <section className={styles.wholeSection}>
       <div className={styles.header}>
@@ -27,35 +26,56 @@ function LoginForm(props) {
         <button className={styles.signUpBtn}>Signup</button>
       </div>
 
-      <Formik onSubmit={submitForm} className={styles.formik} validationSchema={LOGIN_SCHEME} initialValues={initialValues}>
-        <Form className={styles.loginForm}>
-          <h1 className={styles.formHeading}>LOGIN TO YOUR ACCOUNT</h1>
-          <Field
-            className={styles.field}
-            name="login"
-            type="text"
-            placeholder="Your login"
-          ></Field>
-          <ErrorMessage name="login">
-            {(message) => <div className={styles.error}>{message}</div>}
-          </ErrorMessage>
-          <Field
-            className={styles.field}
-            name="password"
-            type="password"
-            placeholder="Password"
-          ></Field>
-          <div className={styles.afterFormWrapper}>
-            <div className={styles.checkbox}>
-              <Field type="checkbox" name="isRemembered" />{" "}
-              <p className={styles.checkboxText} >Remember me</p>{" "}
-            </div>
-            <div>
-              <a  className={styles.passwordLink} href="#">Forgot pasword</a>
-            </div>
-          </div>
-          <button className={styles.logBtn}  type="submit" >LOGIN</button>
-        </Form>
+      <Formik
+        onSubmit={submitForm}
+        className={styles.formik}
+        validationSchema={LOGIN_SCHEME}
+        initialValues={initialValues}
+        validateOnChange={false}
+      >
+        {(formikProps) => {
+          console.log(formikProps);
+
+          const stylesValid = cx(styles.field, {
+            [styles.field]: !formikProps.errors.login,
+            [styles.invalidField]:
+              formikProps.errors.login && formikProps.touched,
+          });
+          return (
+            <Form className={styles.loginForm}>
+              <h1 className={styles.formHeading}>LOGIN TO YOUR ACCOUNT</h1>
+              <Field
+                className={stylesValid}
+                name="login"
+                type="text"
+                placeholder="Your login"
+              ></Field>
+              <ErrorMessage name="login">
+                {(message) => <div className={styles.error}>{message}</div>}
+              </ErrorMessage>
+              <Field
+                className={styles.field}
+                name="password"
+                type="password"
+                placeholder="Password"
+              ></Field>
+              <div className={styles.afterFormWrapper}>
+                <div className={styles.checkbox}>
+                  <Field type="checkbox" name="isRemembered" />{" "}
+                  <p className={styles.checkboxText}>Remember me</p>{" "}
+                </div>
+                <div>
+                  <a className={styles.passwordLink} href="#">
+                    Forgot pasword
+                  </a>
+                </div>
+              </div>
+              <button className={styles.logBtn} type="submit">
+                LOGIN
+              </button>
+            </Form>
+          );
+        }}
       </Formik>
     </section>
   );
