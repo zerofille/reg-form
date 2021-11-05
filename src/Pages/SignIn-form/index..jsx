9709dde3e6a86f9f76f630c8588a98da4e97f4e4
zerofille <nstrdnv@gmai.com>
2021-11-05 18:38:31 +0200

    Add fix

--- src/Pages/SignIn-form/index..jsx
+++ src/Pages/SignIn-form/index..jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import styles from "./style.module.scss";
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { LOGIN_SCHEME } from "../../Utils/Validation-schemas";
+import cx from "classnames";
 function SigInForm() {
-  return <div>1</div>;
+  return (
+    <section className={styles.wholeSection}>
+      <div className={styles.header}>
+        <a className={styles.logo} href="/">
+          <img src="https://www.squadhelp.com/img/logo.png" />
+        </a>
+        <a className={styles.logBtn} href="/login">
+          Login
+        </a>
+      </div>
+      <Formik
+        className={styles.formik}
+        validationSchema={LOGIN_SCHEME}
+        validateOnChange={false}
+      >
+        {(formikProps) => {
+          console.log(formikProps);
+
+          return (
+            <Form className={styles.loginForm}>
+              <h1 className={styles.formHeading}>CREATE AN ACCOUNT</h1>
+              <div className={styles.allFieldsWrap}>
+                <div className={styles.pairingFields}>
+                <Field  className={styles.regForm} name="login" placeholder="Your login"></Field>
+                <Field name="password" placeholder="Password"></Field>
+                </div>
+                <div className={styles.pairingFields}>
+                <Field name="password" placeholder="Password"></Field>
+                <Field name="password" placeholder="Password"></Field>
+                </div>
+                <div className={styles.pairingFields}>
+                <Field name="password" placeholder="Password"></Field>
+                <Field name="password" placeholder="Password"></Field>
+                </div>
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
+    </section>
+  );
 }
 export default SigInForm;
