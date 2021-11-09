5431451b15c99405d957500beb0917d9268b9b69
zerofille <nstrdnv@gmai.com>
2021-11-09 23:32:50 +0200

    Add styles

--- src/Pages/SignIn-form/index..jsx
+++ src/Pages/SignIn-form/index..jsx
@@ -5,14 +5,14 @@ import { SIGN_IN_SCHEME } from "../../Utils/Validation-schemas";
 import cx from "classnames";
 
 const InitialValues = {
-  first_name:"",
-  last_name:"",
-  display_name:"",
-  email:"",
-  password:"",
-  isJoined:false,
-  isAllowed:false
-}
+  first_name: "",
+  last_name: "",
+  display_name: "",
+  email: "",
+  password: "",
+  isJoined: false,
+  isAllowed: false,
+};
 
 function SigInForm() {
   return (
@@ -25,53 +25,160 @@ function SigInForm() {
           Login
         </a>
       </div>
-      <Formik
-        className={styles.formik}
-        validationSchema={SIGN_IN_SCHEME}
-        validateOnChange={false}
-        initialValues={InitialValues}
-      >
-        {(formikProps) => {
-          console.log(formikProps);
+      <div className={styles.formik}>
+        <Formik
+          validationSchema={SIGN_IN_SCHEME}
+          validateOnChange={false}
+          initialValues={InitialValues}
+        >
+          {(formikProps) => {
+            console.log(formikProps);
+            // const stylesErrorsFirst = cx({
+            //   [styles.firstErrorsHidden]: !formikProps.errors.first_name,
+            //   [styles.firstError]:
+            //     formikProps.errors.first_name && formikProps.touched.login,
+            // });
+            // const stylesErrorsSec = cx({
+            //   [styles.firstErrorsHidden]: !formikProps.errors.last_name,
+            //   [styles.firstError]:
+            //     formikProps.errors.last_name && formikProps.touched.login,
+            // });
 
-          return (
-            <Form className={styles.loginForm}>
-              <h1 className={styles.formHeading}>CREATE AN ACCOUNT</h1>
-              <h2 className={styles.formPhrase}>We always keep your name and email address private.</h2>
-              <div className={styles.allFieldsWrap}>
-                <div className={styles.pairingFields}>
-                <Field  className={styles.regForm} name="first_name" placeholder="First name"></Field>
-                <Field  className={styles.regForm} name="last_name" placeholder="Last name"></Field>
+            return (
+              <Form className={styles.loginForm}>
+                <h1 className={styles.formHeading}>CREATE AN ACCOUNT</h1>
+                <h2 className={styles.formPhrase}>
+                  We always keep your name and email address private.
+                </h2>
+                <div className={styles.allFieldsWrap}>
+                  <div className={styles.pairingFields}>
+                    <Field
+                      className={styles.regForm}
+                      name="first_name"
+                      placeholder="First name"
+                    ></Field>
+                    <Field
+                      className={styles.regForm}
+                      name="last_name"
+                      placeholder="Last name"
+                    ></Field>
+                  </div>
+
+                  <ErrorMessage name="first_name">
+                    {(message) => (
+                      <div className={styles.errorMes}>{message}</div>
+                    )}
+                  </ErrorMessage>
+
+                  <ErrorMessage name="last_name">
+                    {(message) => (
+                      <div className={styles.errorMes}>{message}</div>
+                    )}
+                  </ErrorMessage>
+
+                  <div className={styles.pairingFields}>
+                    <Field
+                      className={styles.regForm}
+                      name="display_name"
+                      placeholder="Display name"
+                    ></Field>
+                    <Field
+                      className={styles.regForm}
+                      name="email"
+                      placeholder="Email adress"
+                    ></Field>
+                  </div>
+                  <ErrorMessage name="display_name">
+                    {(message) => (
+                      <div className={styles.errorMes}>{message}</div>
+                    )}
+                  </ErrorMessage>
+
+                  <ErrorMessage name="email">
+                    {(message) => (
+                      <div className={styles.errorMes}>{message}</div>
+                    )}
+                  </ErrorMessage>
+                  <div className={styles.pairingFields}>
+                    <Field
+                      className={styles.regForm}
+                      name="password"
+                      placeholder="Password"
+                    ></Field>
+                    <Field
+                      className={styles.regForm}
+                      name="passwordConf"
+                      placeholder="Password confirmation"
+                    ></Field>
+                  </div>
+                  <ErrorMessage name="password">
+                    {(message) => (
+                      <div className={styles.errorMes}>{message}</div>
+                    )}
+                  </ErrorMessage>
+
+                  <ErrorMessage name="email_name">
+                    {(message) => (
+                      <div className={styles.errorMes}>{message}</div>
+                    )}
+                  </ErrorMessage>
                 </div>
-                <div className={styles.pairingFields}>
-                <Field className={styles.regForm} name="display_name" placeholder="Display name"></Field>
-                <Field className={styles.regForm} name="email" placeholder="Email adress"></Field>
+                <div className={styles.checkboxDiv}>
+                  <Field
+                    className={styles.checkboxRadio}
+                    type="radio"
+                    value="buyer"
+                    name="isJoined"
+                    required
+                  ></Field>
+                  <h1 className={styles.confirmationText}>Join as a Buyer</h1>
+                  <p className={styles.confirmationText2}>
+                    I am looking for a Name, Logo or Tagline for my business,
+                    brand or product.
+                  </p>
                 </div>
-                <div className={styles.pairingFields}>
-                <Field className={styles.regForm} name="password" placeholder="Password"></Field>
-                <Field className={styles.regForm} name="passwordConf" placeholder="Password confirmation"></Field>
+                <div className={styles.checkboxDiv}>
+                  <Field
+                    className={styles.checkboxRadio}
+                    type="radio"
+                    value="seller"
+                    name="isJoined"
+                    required
+                  ></Field>
+                  <h1 className={styles.confirmationText}>
+                    Join As a Creative or Marketplace Seller
+                  </h1>
+                  <p className={styles.confirmationText2}>
+                    I plan to submit name ideas, Logo designs or sell names in
+                    Domain Marketplace.
+                  </p>
                 </div>
-              </div>
-              <div className={styles.checkboxDiv}>
-                <Field className={styles.checkboxRadio}  type='radio' value="buyer" name="isJoined" required></Field>
-                <h1 className={styles.confirmationText}>Join as a Buyer</h1>
-                <p className={styles.confirmationText2}>I am looking for a Name, Logo or Tagline for my business, brand or product.</p>
-              </div>
-              <div className={styles.checkboxDiv}>
-                <Field className={styles.checkboxRadio} type='radio' value="seller" name="isJoined" required></Field>
-                <h1 className={styles.confirmationText}>Join As a Creative or Marketplace Seller</h1>
-                <p className={styles.confirmationText2}>I plan to submit name ideas, Logo designs or sell names in Domain Marketplace.</p>
-              </div>
-              <div className={styles.allow}>
-               <Field type='checkbox' className={styles.allowCheckbox} name="isAllowed" required></Field> 
-              <p className={styles.allowText}>Allow Squadhelp to send marketing/promotional offers from time to time</p>
-              </div>
-              <button className={styles.createBtn}>Create account</button>
-              <p className={styles.terms}>By clicking this button, you agree to our <a className={styles.termsLink} href="#">Terms of Service.</a></p>
-            </Form>
-          );
-        }}
-      </Formik>
+                <div className={styles.allow}>
+                  <Field
+                    type="checkbox"
+                    className={styles.allowCheckbox}
+                    name="isAllowed"
+                    required
+                  ></Field>
+                  <p className={styles.allowText}>
+                    Allow Squadhelp to send marketing/promotional offers from
+                    time to time
+                  </p>
+                </div>
+
+                <button className={styles.createBtn}>Create account</button>
+
+                <p className={styles.terms}>
+                  By clicking this button, you agree to our{" "}
+                  <a className={styles.termsLink} href="#">
+                    Terms of Service.
+                  </a>
+                </p>
+              </Form>
+            );
+          }}
+        </Formik>
+      </div>
     </section>
   );
 }
--- src/Utils/Validation-schemas/index.js
+++ src/Utils/Validation-schemas/index.js
@@ -4,14 +4,14 @@ export const LOGIN_SCHEME = yup.object({
   login: yup
     .string()
     .matches(/^[a-z0-9_-]{3,15}$/, "Inappropriate login")
-    .required("Login is a required fieled"),
+    .required("Login is a required field"),
   password: yup
     .string()
     .matches(
       /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
       "Password must contain 8 - 32 characters in upper- and lower- case, number and special character"
     )
-    .required("Password is a required fieled"),
+    .required("Password is a required field"),
 });
 export const SIGN_IN_SCHEME = yup.object({
   first_name: yup
@@ -20,27 +20,27 @@ export const SIGN_IN_SCHEME = yup.object({
       /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/,
       "Inappropriate first name"
     )
-    .required("First name is a required fieled"),
+    .required("First name is a required field"),
   last_name: yup
     .string()
     .matches(
       /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/,
       "Inappropriate last name"
     )
-    .required("Last name is a required fieled"),
+    .required("Last name is a required field"),
   display_name: yup
     .string()
     .matches(/^[a-z0-9_-]{3,15}$/, "Inappropriate display name")
-    .required("Display name is a required fieled"),
+    .required("Display name is a required field"),
   email: yup
     .string()
     .matches(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, "Inappropriate email")
-    .required("Display name is a required fieled"),
+    .required("Email is a required fieled"),
   password: yup
     .string()
     .matches(
       /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
       "Password must contain 8 - 32 characters in upper- and lower- case, number and special character"
     )
-    .required("Password is a required fieled"),
+    .required("Password is a required field"),
 });
